569a79197c848eb3b904e5c343bb20a4addeb73a
Chad Jones <chadjones839@gmail.com>
2020-08-26 15:27:54 -0500

    Urban Planner I

---
+++ Building.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Planner
+{
+  public class Building
+  {
+    private string _designer { get; set; }
+    private DateTime _dateConstructed { get; set; }
+    private string _address { get; set; }
+    private string _owner { get; set; }
+
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume
+    {
+      get { return Width * Depth * (3 * Stories); }
+    }
+
+
+    public Building(string Address) => _address = Address;
+
+    public void Construct(string Designer, string Owner) 
+    {
+      _dateConstructed = DateTime.Now;
+      _designer = Designer;
+      _owner = Owner;
+    }
+
+    public void ListBuildings()
+    {
+      Console.WriteLine($"{_address}");
+      Console.WriteLine("-------------");
+      Console.WriteLine($"Designed by {_designer}");
+      Console.WriteLine($"Constructed on {_dateConstructed}");
+      Console.WriteLine($"Owned by {_owner}");
+      Console.WriteLine($"{Volume} cubic meters of space");
+      Console.WriteLine();
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building abstergo = new Building("Abstergo Inc.");
+            abstergo.Construct("Ongo Gablogian", "Durl Winston");
+            abstergo.Stories = 120;
+            abstergo.Width = 300.5;
+            abstergo.Depth = 150.5;
+
+            Building spacersChoice = new Building("Spacers Choice");
+            spacersChoice.Construct("Ongo Gablogian", "Wilson Brown");
+            spacersChoice.Stories = 85;
+            spacersChoice.Width = 100.3;
+            spacersChoice.Depth = 100.8;
+
+            Building ominent = new Building("Ominent Labs");
+            ominent.Construct("Ongo Gablogian", "Carl Hanratty");
+            ominent.Stories = 12;
+            ominent.Width = 200.5;
+            ominent.Depth = 235.5;
+
+            abstergo.ListBuildings();
+            spacersChoice.ListBuildings();
+            ominent.ListBuildings();
+        }
+    }
+}
